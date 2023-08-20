862e546cee7505676f6fb2ce4fcdc1bc9f4431c7
Wendy Randolph <wendy.randolph5@gmail.com>
2023-08-19 23:19:43 -0400

    Order index rows by image first

--- src/fabric.clj
+++ src/fabric.clj
@@ -6,7 +6,7 @@
 (defn index [request]
   (let [rows (coast/q '[:select *
                         :from fabric
-                        :order yards desc structure desc color desc 
+                        :order image desc yards desc structure desc color desc 
                         #_#__:limit 20])]
     (container {:mw 8}
      (when (not (empty? rows))
