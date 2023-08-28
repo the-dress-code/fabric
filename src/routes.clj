1ec50afc786210925fd91a92dfdd3d5f9fbffb70
Wendy Randolph <wendy.randolph5@gmail.com>
2023-08-27 21:10:17 -0400

    Create new routes and auth middleware

--- src/routes.clj
+++ src/routes.clj
@@ -1,17 +1,31 @@
 (ns routes
   (:require [coast]
-            [components]))
+            [components]
+            [middleware]))
+
 
 (def routes
   (coast/routes
 
     (coast/site
-      (coast/with-layout components/layout
+
+     [:get "/sign-up" :member/build]
+     [:post "/members" :member/create]
+     [:get "/sign-in" :session/build]
+     [:post "/sessions" :session/create]
+
+     (coast/with middleware/auth
+
+        [:get "/dashboard" :member/dashboard]
+        [:delete "/sessions" :session/delete])
+
+     (coast/with-layout components/layout
         [:get "/" :site.home/index]
         [:get "/fabrics/search" :fabric/search]
         [:get "/fabrics/answers" :fabric/answers]
         [:resource :fabric]))
 
     (coast/api
+
       (coast/with-prefix "/api"
         [:get "/" :api.home/index]))))
--- src/server.clj
+++ src/server.clj
@@ -1,3 +1,4 @@
+
 (ns server
   (:require [coast]
             [routes])
