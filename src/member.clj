b61d6ee1bac4899ad5208751305cb8929f55cfde
Wendy Randolph <wendy.randolph5@gmail.com>
2023-08-30 21:31:47 -0400

    Use tap> to inspect binding in create fn in member

--- src/member.clj
+++ src/member.clj
@@ -28,3 +28,41 @@
    [:h1 "You're signed in! Welcome to your fabric stash!"]
    (coast/form-for :session/delete
     [:input {:type "submit" :value "Sign out"}])])
+
+
+(comment
+
+(def debug-m (atom nil))
+
+(add-tap #(reset! debug-m %))
+
+@debug-m
+; on params request on create
+;; => {:__anti-forgery-token
+;;     "A4kKk1KFHi7INThtqQsXCOrFP3J0Ts+OYilfc/4KKmhRJoe/PZntPVHR9gly8UsnZCke3E9/BrDSCxI9",
+;;     :member/email "signuptest908@gmail.com",
+;;     :member/password "hello"}
+
+(select-keys @debug-m [:member/email :member/password])
+;; => #:member{:email "signuptest908@gmail.com", :password "hello"}
+
+(coast/validate  #:member{:email "signuptest908@gmail.com", :password "hello"} 
+                 [[:email [:member/email]
+                          [:required [:member/email :member/password]]]])
+;; => #:member{:email "signuptest908@gmail.com", :password "hello"}
+
+(update #:member{:email "signuptest908@gmail.com", :password "hello"} :member/password hashers/derive)
+;; => #:member{:email "signuptest908@gmail.com",
+;;             :password
+;;             "bcrypt+sha512$9970ca4b87c21df69b48138e379f393b$12$a6406dde27b1b55443f4158684649fee5d8ff5a17110136c"}
+
+(coast/rescue  #:member{:email "signuptest908@gmail.com",
+                :password      "bcrypt+sha512$9970ca4b87c21df69b48138e379f393b$12$a6406dde27b1b55443f4158684649fee5d8ff5a17110136c"})
+;; => [#:member{:email "signuptest908@gmail.com",
+;;              :password
+;;              "bcrypt+sha512$9970ca4b87c21df69b48138e379f393b$12$a6406dde27b1b55443f4158684649fee5d8ff5a17110136c"}
+;;     nil]
+
+
+
+)
