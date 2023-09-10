f48dfa6510158c1516dd2ffe830ce1577d90c076
Wendy Randolph <wendy.randolph5@gmail.com>
2023-09-09 23:26:41 -0400

    Label sign up page, check for existing member email, provide error message if exists.

--- src/member.clj
+++ src/member.clj
@@ -4,29 +4,37 @@
 
 
 (defn build [request]
-  (coast/form-for ::create
-                  [:input {:type "text" :name "member/email" :required true}]
-                  [:input {:type "password" :name "member/password" :required true}]
-                  [:input {:type "submit" :value "Submit"}]))
+  [:div 
+   [:h1 "Sign up, and start stashin'!"]
+    (when (some? (:error/message request))
+      [:div (:error/message request)])
+    (coast/form-for ::create
+                   [:input {:type "text" :name "member/email" :required true}]
+                   [:input {:type "password" :name "member/password" :required true}]
+                   [:input {:type "submit" :value "Submit"}])])
 
 
 (defn create [request]
-  (let [[_ errors] (-> (:params request)
-                       (select-keys [:member/email :member/password])
-                       (coast/validate [[:email [:member/email]
-                                        [:required [:member/email :member/password]]]])
-                       (update :member/password hashers/derive)
-                       (coast/insert)
-                       (coast/rescue))]
-    (if (some? errors)
-      (build (merge errors request))
-      (-> (coast/redirect-to ::dashboard)
-          (assoc :session (select-keys (:params request) [:member/email]))))))
+  (let [email (get-in request [:params :member/email])
+        member (coast/find-by :member {:email email})]
+    (if member
+      (build (merge request {:error/message "Email address already exists"}))
+      (let [[_ errors] (-> (:params request)
+                           (select-keys [:member/email :member/password])
+                           (coast/validate [[:email [:member/email]
+                                             [:required [:member/email :member/password]]]])
+                           (update :member/password hashers/derive)
+                           (coast/insert)
+                           (coast/rescue))]
+        (if (some? errors)
+          (build (merge errors request))
+          (-> (coast/redirect-to ::dashboard)
+              (assoc :session (select-keys (:params request) [:member/email]))))))))
 
 
 (defn dashboard [request]
   [:div 
-   [:h1 "You're signed in! Welcome to your fabric stash!"]
+   [:H1 "You're signed in! Welcome to your fabric stash!"]
    (coast/form-for :session/delete
     [:input {:type "submit" :value "Sign out"}])])
 
@@ -36,6 +44,7 @@
 (def debug-m (atom nil))
 
 (add-tap #(reset! debug-m %))
+;; => nil
 
 @debug-m
 
