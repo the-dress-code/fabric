3c353b637d13d8e3174e6aab531fda76ac01b98d
Wendy Randolph <wendy.randolph5@gmail.com>
2023-09-20 22:50:01 -0500

    Test queries for working with email and id

--- test/queries.clj
+++ test/queries.clj
@@ -3,6 +3,41 @@
             [components]
             [fabric]))
 
+
+(coast/q '[:select id
+                        :from member
+                          :where [:email ?email]]
+                        {:email "charles@barkley.com"})
+;; => ({:id 21})
+
+
+(coast/q '[:select id
+           :from member])
+;; => ({:id 1}
+;;     {:id 2}
+;;     {:id 3}
+;;     {:id 4}
+;;     {:id 5}
+;;     {:id 6}
+;;     {:id 7}
+;;     {:id 8}
+;;     {:id 9}
+;;     {:id 10}
+;;     {:id 11}
+;;     {:id 12}
+;;     {:id 13}
+;;     {:id 14}
+;;     {:id 15}
+;;     {:id 16}
+;;     {:id 17}
+;;     {:id 18}
+;;     {:id 19})
+
+(map :id (coast/q '[:select id
+                     :from member
+                     :where [:email "wendy@804.pm"]]))
+;; => (1)
+
 (coast/q '[:select * 
            :from fabric
            :order yards desc])
