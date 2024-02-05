bd780f65c549d2af4ae923c608017141c26f011e
Wendy Randolph <wendy.randolph5@gmail.com>
2024-02-04 21:16:42 -0500

    Create gate-keeper middleware fn to restrict access to non-public routes

--- src/middleware.clj
+++ src/middleware.clj
@@ -7,3 +7,26 @@
     (if (some? (get-in request [:session :member/email]))
       (handler request)
       (coast/unauthorized "HAL9000 says: I'm sorry Wendy, I can't let you do that"))))
+
+(defn gate-keeper [handler]
+  (fn [request]
+    (let [public-routes {"/sign-up" :public 
+                         "/members" :public
+                         "/login" :public
+                         "/sessions" :public}]
+      (if (= :public (get public-routes (:uri request)))
+        (handler request)
+        (coast/unauthorized "No.")))))
+
+;; update line 19 to the following:
+#_(coast/redirect-to :member/build)
+
+(comment
+
+(def debug-m (atom nil))
+
+(add-tap #(reset! debug-m %))
+
+@debug-m
+
+)
