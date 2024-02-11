2e208ec2ed319ef9ab21c748f70cc2d6a19284fa
Wendy Randolph <wendy.randolph5@gmail.com>
2024-02-10 21:38:37 -0500

    Add build

---
+++ src/build.clj
@@ -0,0 +1,23 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'com.github.the-dress-code/fabric)
+(def version (format "1.2.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
+(def basis (delay (b/create-basis {:project "deps.edn"})))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn uber [_]
+  (clean nil)
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/compile-clj {:basis @basis
+                  :ns-compile '[server]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis @basis
+           :main 'server}))
