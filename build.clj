1c61f35a8d3c59875a98d13e1cef10f7eef30191
Wendy Randolph <wendy.randolph5@gmail.com>
2024-02-13 11:25:48 -0500

    Add db dir to uber function

--- build.clj
+++ build.clj
@@ -12,7 +12,7 @@
 
 (defn uber [_]
   (clean nil)
-  (b/copy-dir {:src-dirs ["src" "resources"]
+  (b/copy-dir {:src-dirs ["src" "db" "resources"]
                :target-dir class-dir})
   (b/compile-clj {:basis @basis
                   :ns-compile '[server]
